a24bf8948a99ba2b7373d46d3cbe152c65afdda5
Naizil Francis D Costa <112805712+Naizil-Francis@users.noreply.github.com>
2024-07-04 10:10:13 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,147 +1,88 @@
-import numpy as np
-import pandas as pd
+import os
+import sys
+import transformers
 import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, LSTM, Dense
-from sklearn.model_selection import train_test_split
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from transformers import TFAutoModelForSeq2SeqLM, DataCollatorForSeq2Seq
+from transformers import AdamWeightDecay
+from transformers import AutoTokenizer, TFAutoModelForSeq2SeqLM
 
-# Sample dataset
-data = {
-    'english': ['hello', 'how are you', 'thank you', 'goodbye'],
-    'kannada': ['hello', 'hege iddira', 'Dhanyavadagalu', 'vidaaya']
-}
-df = pd.DataFrame(data)
+model_checkpoint = "Helsinki-NLP/opus-mt-en-hi"
 
-# Preprocessing
-def preprocess_data(df):
-    input_texts = []
-    target_texts = []
-    input_characters = set()
-    target_characters = set()
+raw_datasets = load_dataset("cfilt/iitb-english-hindi")
 
-    for idx, row in df.iterrows():
-        input_text = row['english']
-        target_text = '\t' + row['kannada'] + '\n'
-        
-        input_texts.append(input_text)
-        target_texts.append(target_text)
-        
-        for char in input_text:
-            if char not in input_characters:
-                input_characters.add(char)
-        for char in target_text:
-            if char not in target_characters:
-                target_characters.add(char)
-                
-    input_characters = sorted(list(input_characters))
-    target_characters = sorted(list(target_characters))
+print(raw_datasets)
 
-    num_encoder_tokens = len(input_characters)
-    num_decoder_tokens = len(target_characters)
-    max_encoder_seq_length = max([len(txt) for txt in input_texts])
-    max_decoder_seq_length = max([len(txt) for txt in target_texts])
+print(raw_datasets['train'][1])
 
-    input_token_index = dict([(char, i) for i, char in enumerate(input_characters)])
-    target_token_index = dict([(char, i) for i, char in enumerate(target_characters)])
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
-    encoder_input_data = np.zeros((len(input_texts), max_encoder_seq_length, num_encoder_tokens), dtype='float32')
-    decoder_input_data = np.zeros((len(input_texts), max_decoder_seq_length, num_decoder_tokens), dtype='float32')
-    decoder_target_data = np.zeros((len(input_texts), max_decoder_seq_length, num_decoder_tokens), dtype='float32')
+print(tokenizer("Hello, this is a sentence!"))
 
-    for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
-        for t, char in enumerate(input_text):
-            encoder_input_data[i, t, input_token_index[char]] = 1.
-        for t, char in enumerate(target_text):
-            decoder_input_data[i, t, target_token_index[char]] = 1.
-            if t > 0:
-                decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+max_input_length = 128
+max_target_length = 128
 
-    return encoder_input_data, decoder_input_data, decoder_target_data, input_token_index, target_token_index, num_encoder_tokens, num_decoder_tokens, max_encoder_seq_length, max_decoder_seq_length
+source_lang = "en"
+target_lang = "hi"
 
-encoder_input_data, decoder_input_data, decoder_target_data, input_token_index, target_token_index, num_encoder_tokens, num_decoder_tokens, max_encoder_seq_length, max_decoder_seq_length = preprocess_data(df)
 
-# Model building
-latent_dim = 256
+def preprocess_function(examples):
+    inputs = [ex[source_lang] for ex in examples["translation"]]
+    targets = [ex[target_lang] for ex in examples["translation"]]
+    model_inputs = tokenizer(inputs, max_length=max_input_length, truncation=True)
 
-# Encoder
-encoder_inputs = Input(shape=(None, num_encoder_tokens))
-encoder = LSTM(latent_dim, return_state=True)
-encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-encoder_states = [state_h, state_c]
+    # Setup the tokenizer for targets
+    with tokenizer.as_target_tokenizer():
+        labels = tokenizer(targets, max_length=max_target_length, truncation=True)
 
-# Decoder
-decoder_inputs = Input(shape=(None, num_decoder_tokens))
-decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
-decoder_dense = Dense(num_decoder_tokens, activation='softmax')
-decoder_outputs = decoder_dense(decoder_outputs)
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
 
-# Define the model that will turn encoder_input_data & decoder_input_data into decoder_target_data
-model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+preprocess_function(raw_datasets["train"][:2])
+tokenized_datasets = raw_datasets.map(preprocess_function, batched=True)
+model = TFAutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
 
-# Compile the model
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+batch_size = 16
+learning_rate = 2e-5
+weight_decay = 0.01
+num_train_epochs = 1
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model, return_tensors="tf")
+generation_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model, return_tensors="tf", pad_to_multiple_of=128)
+train_dataset = model.prepare_tf_dataset(
+    tokenized_datasets["test"],
+    batch_size=batch_size,
+    shuffle=True,
+    collate_fn=data_collator,
+)
+validation_dataset = model.prepare_tf_dataset(
+    tokenized_datasets["validation"],
+    batch_size=batch_size,
+    shuffle=False,
+    collate_fn=data_collator,
+)
+generation_dataset = model.prepare_tf_dataset(
+    tokenized_datasets["validation"],
+    batch_size=8,
+    shuffle=False,
+    collate_fn=generation_data_collator,
+)
+optimizer = AdamWeightDecay(learning_rate=learning_rate, weight_decay_rate=weight_decay)
+model.compile(optimizer=optimizer)
 
-# Train the model
-model.fit([encoder_input_data, decoder_input_data], decoder_target_data, batch_size=64, epochs=100, validation_split=0.2)
+model.fit(train_dataset, validation_data=validation_dataset, epochs=1)
+model.save_pretrained("tf_model/")
 
-# Define sampling models
-encoder_model = Model(encoder_inputs, encoder_states)
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = TFAutoModelForSeq2SeqLM.from_pretrained("tf_model/")
 
-decoder_state_input_h = Input(shape=(latent_dim,))
-decoder_state_input_c = Input(shape=(latent_dim,))
-decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+input_text  = "hello India"
 
-decoder_outputs, state_h, state_c = decoder_lstm(
-    decoder_inputs, initial_state=decoder_states_inputs)
-decoder_states = [state_h, state_c]
-decoder_outputs = decoder_dense(decoder_outputs)
-decoder_model = Model(
-    [decoder_inputs] + decoder_states_inputs,
-    [decoder_outputs] + decoder_states)
+tokenized = tokenizer([input_text], return_tensors='np')
+out = model.generate(**tokenized, max_length=128)
+print(out)
 
-# Reverse-lookup token index to decode sequences back to readable form
-reverse_input_char_index = dict((i, char) for char, i in input_token_index.items())
-reverse_target_char_index = dict((i, char) for char, i in target_token_index.items())
+with tokenizer.as_target_tokenizer():
+    print(tokenizer.decode(out[0], skip_special_tokens=True))
 
-# Decode sequence
-def decode_sequence(input_seq):
-    # Encode the input as state vectors
-    states_value = encoder_model.predict(input_seq)
 
-    # Generate empty target sequence of length 1
-    target_seq = np.zeros((1, 1, num_decoder_tokens))
-    # Populate the first character of target sequence with the start character
-    target_seq[0, 0, target_token_index['\t']] = 1.
-
-    # Sampling loop for a batch of sequences
-    stop_condition = False
-    decoded_sentence = ''
-    while not stop_condition:
-        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
-
-        # Sample a token
-        sampled_token_index = np.argmax(output_tokens[0, -1, :])
-        sampled_char = reverse_target_char_index[sampled_token_index]
-        decoded_sentence += sampled_char
-
-        # Exit condition: either hit max length or find stop character
-        if (sampled_char == '\n' or len(decoded_sentence) > max_decoder_seq_length):
-            stop_condition = True
-
-        # Update the target sequence (length 1)
-        target_seq = np.zeros((1, 1, num_decoder_tokens))
-        target_seq[0, 0, sampled_token_index] = 1.
-
-        # Update states
-        states_value = [h, c]
-
-    return decoded_sentence
-
-# Test the model
-for seq_index in range(len(df)):
-    input_seq = encoder_input_data[seq_index: seq_index + 1]
-    decoded_sentence = decode_sequence(input_seq)
-    print('-')
-    print('Input sentence:', df['english'][seq_index])
-    print('Decoded sentence:', decoded_sentence)
